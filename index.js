a8407ad5934b730304814d22920a90af64c7733c
roxannesyombua <roxannesyombua@gmail.com>
2024-04-06 10:52:30 +0300

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function () {
+
+    let a = 2000
+    let b = 100
+    return a / b
+};
+console.log(divide(a, b));
+
+const square = (x) => x * x;
+let x = 2
+square(2);
+
+const add = (a,b) => a + b
+let a = 3
+let b = 4
+
+console.log(a + b);
